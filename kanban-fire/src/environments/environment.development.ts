27c3cd5fc532d59fa743341925ba01969d03e004
AndyZavelinski <zavelinski20@gmail.com>
2023-04-19 15:39:26 -0300

    Kanban

---
+++ kanban-fire/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'cursofirebase-ceecc',
+    appId: '1:78815783268:web:5867e95594e5029d11b271',
+    storageBucket: 'cursofirebase-ceecc.appspot.com',
+    apiKey: 'AIzaSyD9KqZVPnPt5hSFmIv4M7Riwirb-PIi-cA',
+    authDomain: 'cursofirebase-ceecc.firebaseapp.com',
+    messagingSenderId: '78815783268',
+    measurementId: 'G-QDRTR8PQR9',
+  },};
---
+++ kanban-fire/src/environments/environment.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXb5VdRRNi4Wfs_rjjk5AJNHNMkLrWrEU",
+  authDomain: "kanbanfire-8629e.firebaseapp.com",
+  databaseURL: "https://kanbanfire-8629e-default-rtdb.firebaseio.com",
+  projectId: "kanbanfire-8629e",
+  storageBucket: "kanbanfire-8629e.appspot.com",
+  messagingSenderId: "863916798948",
+  appId: "1:863916798948:web:b3bc5267a75931ceae8df1",
+  measurementId: "G-0Y6KR4CK3Z"
+};
